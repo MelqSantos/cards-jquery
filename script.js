0d3398de8a5fd0b959f4edb6153b61af8ca0039f
Melqui Santos <melqsantos96@gmail.com>
2022-10-21 16:05:41 -0300

    Correção de texto

--- script.js
+++ script.js
@@ -8,29 +8,30 @@ $(document).ready(function() {
         },
         {
             titulo: "Texto 02",
-            comentario: "Programação com Jquery 01 <br> Simulando requisição GET..."
+            comentario: "Programação com Jquery 02 <br> Simulando requisição GET..."
         },
         {
             titulo: "Texto 03",
-            comentario: "Programação com Jquery 01 <br> Simulando requisição GET..."
+            comentario: "Programação com Jquery 03 <br> Simulando requisição GET..."
         },
         {
             titulo: "Texto 04",
-            comentario: "Programação com Jquery 01 <br> Simulando requisição GET..."
+            comentario: "Programação com Jquery 04 <br> Simulando requisição GET..."
         }
     ];
 
     // Cria os elementos dinamicamente
     for(i = 0; i < texto.length; i++){
 
-       var card = $('<div class="card">' +
-            '<div class="title">' +
-                '<h3>' + texto[i].titulo + '</h3>' +
-            '</div>' +
-            '<div class="body">' +
-                '<p>' + texto[i].comentario + '</p>' +
-            '</div>' +
-        '</div>');
+       var card = $(
+           '<div class="card">' +
+                '<div class="title">' +
+                    '<h3>' + texto[i].titulo + '</h3>' +
+                '</div>' +
+                '<div class="body">' +
+                    '<p>' + texto[i].comentario + '</p>' +
+                '</div>' +
+            '</div>');
 
         // Atribui os elementos html na div dos cards
         $(".card-wrapper").append(card);
